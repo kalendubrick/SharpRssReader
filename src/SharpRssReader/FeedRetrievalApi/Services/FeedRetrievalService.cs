84bbb0cc599bd404e20681eca2da9210c219ad7a
kalendubrick <kalen@kalendubrick.com>
2022-11-06 11:52:07 -0600

    Initial skeleton of feed service

---
+++ src/SharpRssReader/FeedRetrievalApi/Services/FeedRetrievalService.cs
@@ -0,0 +1,30 @@
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace FeedRetrievalApi.Services;
+
+public class FeedRetrievalService : IFeedRetrievalService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public FeedRetrievalService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<SyndicationFeed> ReadFeedAsync(string feedUrl)
+    {
+        var client = _httpClientFactory.CreateClient();
+
+        using var feedStream = await client.GetStreamAsync(feedUrl);
+
+        if (feedStream is null)
+        {
+            return new SyndicationFeed();
+        }
+
+        using var reader = XmlReader.Create(feedStream);
+
+        return SyndicationFeed.Load(reader);
+    }
+}
---
+++ src/SharpRssReader/FeedRetrievalApi/Services/IFeedRetrievalService.cs
@@ -0,0 +1,9 @@
+﻿using System.ServiceModel.Syndication;
+
+namespace FeedRetrievalApi.Services
+{
+    public interface IFeedRetrievalService
+    {
+        Task<SyndicationFeed> ReadFeedAsync(string feedUrl);
+    }
+}
