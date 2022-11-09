3a2346998af1ed6d7e6c83ca092f967e07cdc580
kalendubrick <kalen@kalendubrick.com>
2022-11-09 07:57:44 -0600

    Add part of first test

--- src/SharpRssReader/FeedRetrievalApi.Tests/FeedRetrievalServiceTests.cs
+++ src/SharpRssReader/FeedRetrievalApi.Tests/FeedRetrievalServiceTests.cs
@@ -1,3 +1,7 @@
+using Moq;
+using Moq.Protected;
+using System.Net;
+
 namespace FeedRetrievalApi.Tests;
 
 public class FeedRetrievalServiceTests
@@ -5,6 +9,20 @@ public class FeedRetrievalServiceTests
     [Fact]
     public void Test1()
     {
+        // Arrange
+        var mockFactory = new Mock<IHttpClientFactory>();
 
+        var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+        mockHttpMessageHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("GetAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(""),
+            });
+
+        // Act
+
+        // Assert
     }
 }
