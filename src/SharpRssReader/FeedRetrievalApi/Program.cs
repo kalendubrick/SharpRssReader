c05fc6fe16f4357d2bc4b3adb3013b70056012c8
kalendubrick <kalen@kalendubrick.com>
2022-11-12 09:08:13 -0600

    Add minimal API

--- src/SharpRssReader/FeedRetrievalApi/Program.cs
+++ src/SharpRssReader/FeedRetrievalApi/Program.cs
@@ -1,4 +1,7 @@
+using FeedRetrievalApi.Exceptions;
 using FeedRetrievalApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.ServiceModel.Syndication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,6 +34,32 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapGet("/feed", async ([FromBody] string feedUrl, [FromServices] FeedRetrievalService feedService, HttpResponse response) =>
+{
+    try
+    {
+        var feed = await feedService.ReadFeedAsync(feedUrl);
+
+        response.StatusCode = StatusCodes.Status200OK;
+        await response.WriteAsJsonAsync(feed);
+    }
+    catch (FeedRequestException e)
+    {
+        response.StatusCode = StatusCodes.Status400BadRequest;
+        await response.WriteAsJsonAsync(e.Message);
+    }
+    catch (FeedEmptyException e)
+    {
+        response.StatusCode = StatusCodes.Status400BadRequest;
+        await response.WriteAsJsonAsync(e.Message);
+    }
+    catch (FeedLoadException e)
+    {
+        response.StatusCode = StatusCodes.Status500InternalServerError;
+        await response.WriteAsJsonAsync(e.Message);
+    }
+})
+.Produces<SyndicationFeed>(StatusCodes.Status200OK)
+.WithName("GetFeed").WithTags("Getters");
 
 app.Run();
