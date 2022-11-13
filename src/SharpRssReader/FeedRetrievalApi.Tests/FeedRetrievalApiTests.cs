23fbea38ab4858a74ab941c967b528b5df8c32f0
Kalen Dubrick <kalen@kalendubrick.com>
2022-11-13 10:56:14 -0600

    Add basic API test

---
+++ src/SharpRssReader/FeedRetrievalApi.Tests/FeedRetrievalApiTests.cs
@@ -0,0 +1,21 @@
+﻿using FeedRetrievalApi.Tests.Models;
+
+namespace FeedRetrievalApi.Tests;
+
+public class FeedRetrievalApiTests
+{
+    [Fact]
+    public async Task Api_should_return_200_on_valid_feed()
+    {
+        // Arrange
+
+        await using var application = new FeedRetrievalApiApplication();
+        var client = application.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("feed?feedUrl=https%3A%2F%2Fcommunity.ricksteves.com%2Ftravel-forum%2Fgermany.atom");
+
+        // Assert
+        Assert.Equal(200, (int)response.StatusCode);
+    }
+}
---
+++ src/SharpRssReader/FeedRetrievalApi.Tests/Models/FeedRetrievalApiApplication.cs
@@ -0,0 +1,19 @@
+﻿using FeedRetrievalApi.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FeedRetrievalApi.Tests.Models;
+
+internal class FeedRetrievalApiApplication : WebApplicationFactory<Program>
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddSingleton<IFeedRetrievalService, FeedRetrievalService>();
+        });
+
+        return base.CreateHost(builder);
+    }
+}
