a96e19a914b44293cb2d99297460272aebf6d4b6
kalendubrick <kalen@kalendubrick.com>
2022-11-19 15:42:14 -0600

    Finish fleshing out models

--- src/SharpRssReader/SharpRssReader.Models/SharpFeedItem.cs
+++ src/SharpRssReader/SharpRssReader.Models/SharpFeedItem.cs
@@ -1,4 +1,26 @@
-﻿namespace SharpRssReader.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace SharpRssReader.Models;
+
 public class SharpFeedItem
 {
+    public Collection<SharpFeedPerson> Authors { get; init; } = new Collection<SharpFeedPerson>();
+
+    public Collection<SharpFeedCategory> Categories { get; init; } = new Collection<SharpFeedCategory>();
+
+    public Collection<SharpFeedPerson> Contributors { get; init; } = new Collection<SharpFeedPerson>();
+
+    public string? Copyright { get; set; }
+
+    public string? Id { get; set; }
+
+    public DateTimeOffset LastUpdatedTime { get; set; }
+
+    public Collection<SharpFeedLink> Links { get; init; } = new Collection<SharpFeedLink>();
+
+    public DateTimeOffset PublishDate { get; set; }
+
+    public string? Summary { get; set; }
+
+    public string? Title { get; set; }
 }
--- src/SharpRssReader/SharpRssReader.Models/SharpFeedLink.cs
+++ src/SharpRssReader/SharpRssReader.Models/SharpFeedLink.cs
@@ -1,4 +1,15 @@
 ﻿namespace SharpRssReader.Models;
 public class SharpFeedLink
 {
+    public Uri BaseUri { get; set; }
+
+    public long Length { get; set; }
+
+    public string MediaType { get; set; }
+
+    public string RelationshipType { get; set; }
+
+    public string Title { get; set; }
+
+    public Uri Uri { get; set; }
 }
